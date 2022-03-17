095c799d5917b00fc2053a7c94ad030380644f5f
valflrt <vfleurit@gmail.com>
2022-03-17 20:25:00 +0100

    fix path in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: `${__dirname}/index.html`,
+      template: `${__dirname}/public/index.html`,
     }),
     new MiniCssExtractPlugin.default(),
   ],
