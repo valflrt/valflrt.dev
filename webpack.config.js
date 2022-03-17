5dd6febed95fa768d5dd4a16e19674f68a96bfe3
valflrt <vfleurit@gmail.com>
2022-03-17 19:09:06 +0100

    move public to root

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: `${__dirname}/src/public/index.html`,
+      template: `${__dirname}/index.html`,
     }),
     new MiniCssExtractPlugin.default(),
   ],
