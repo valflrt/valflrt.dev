958fbde20a69dfdb8289b49d8f3955643d7cb4bf
valflrt <vfleurit@gmail.com>
2022-04-23 19:25:51 +0200

    edit imports

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from "react";
 
-import styles from "./Layout.module.css";
+import styles from "./Layout.module.scss";
 
 const Layout: FC = ({ children }) => (
   <div className={styles.layout}>{children}</div>
