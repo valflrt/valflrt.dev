6ed61c557eb08ee9188216f80696cb6972fafa28
valflrt <vfleurit@gmail.com>
2022-04-24 14:41:08 +0200

    add some image file extensions

--- src/fileExtensions.d.ts
+++ src/fileExtensions.d.ts
@@ -5,3 +5,9 @@
 declare module "*.css";
 declare module "*.scss";
 declare module "*.sass";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.png";
+declare module "*.gif";
+declare module "*.mp3";
+declare module "*.svg";
