eb0ab6c33e7f3958b0737d2bd951244969fe5ad1
valflrt <vfleurit@gmail.com>
2022-04-24 14:51:38 +0200

    add logo and comments in Main route

--- src/routes/Main.tsx
+++ src/routes/Main.tsx
@@ -1,10 +1,20 @@
-import React from "react";
 import { Link } from "react-router-dom";
+
+/**
+ * You can import images
+ */
+import logo from "../assets/logo-128.png";
 
 const Main = () => (
   <>
+    {/* imported image used here */}
+    <img src={logo} alt="logo" />
     <h1>Heya !</h1>
-    <Link to={"/fgsigfds"}>Go to /fgsigfds (404 page test)</Link>
+    <p>This is a react template</p>
+    {/* use react-router-dom `Link` to redirect properly */}
+    <Link to={"/nothing"}>
+      Go to <b>/nothing</b> to test 404 page
+    </Link>
   </>
 );
 
