d26915c482e355041439670528d24fc4796c8cca
valflrt <vfleurit@gmail.com>
2022-04-24 14:52:22 +0200

    edit index.tsx - add comment about HashRouter and BrowerRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,24 @@
 import ReactDOM from "react-dom";
+
 import { HashRouter } from "react-router-dom";
 
-import Layout from "./layout/Layout";
 import Router from "./router/Router";
+import Layout from "./layout/Layout";
 
 import "./globalStyles.css";
 
 ReactDOM.render(
+  /**
+   * HashRouter is a router using the "#" (hash) in the url
+   * (example: `https://md-editor.valflrt.dev/#/editor`) instead of
+   * simple adding slashes, I use it because BrowserRouter
+   * doesn't work with github-pages because it requires a
+   * fallback to index.html in case of 404 error
+   *
+   * If you want to use BrowserRouter instead of HashRouter,
+   * go ahead and replace HashRouter by BrowserRouter in the
+   * code below
+   */
   <HashRouter>
     <Layout>
       <Router />
