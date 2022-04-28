f0a7e03024f1d9062a89155d35c65e9d56e7c196
valflrt <vfleurit@gmail.com>
2022-04-28 21:08:57 +0200

    edit index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -8,17 +8,6 @@ import Layout from "./layout/Layout";
 import "./globalStyles.css";
 
 ReactDOM.render(
-  /**
-   * HashRouter is a router using the "#" (hash) in the url
-   * (example: `https://md-editor.valflrt.dev/#/editor`) instead of
-   * simple adding slashes, I use it because BrowserRouter
-   * doesn't work with github-pages because it requires a
-   * fallback to index.html in case of 404 error
-   *
-   * If you want to use BrowserRouter instead of HashRouter,
-   * go ahead and replace HashRouter by BrowserRouter in the
-   * code below
-   */
   <HashRouter>
     <Layout>
       <Router />
