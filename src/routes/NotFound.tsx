feac6dba051abc2e9e61928b8b95d73c153d1494
valflrt <vfleurit@gmail.com>
2022-05-01 16:32:03 +0200

    edit NotFound

--- src/routes/NotFound.tsx
+++ src/routes/NotFound.tsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
 
+import "./NotFound.scss";
+
 const NotFound = () => (
   <>
-    <h1>404</h1>
+    <h1 className="fortyHundredAndFour">404</h1>
     <p>There's nothing here !</p>
     <Link className={"link"} to={"/"}>
       Back home
