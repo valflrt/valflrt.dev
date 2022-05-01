f2c479121f0d9abf135859a9aa19b6fbebd6c129
valflrt <vfleurit@gmail.com>
2022-05-01 16:12:41 +0200

    add Menu

---
+++ src/components/Menu.tsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+import "./Menu.scss";
+
+const Menu = () => (
+  <div className={"menu"}>
+    <Link className={"link"} to="/">
+      Home
+    </Link>
+
+    <Link className={"link"} to="/projects">
+      Projects
+    </Link>
+
+    <Link className={"link"} to="/social">
+      Social
+    </Link>
+  </div>
+);
+
+export default Menu;
