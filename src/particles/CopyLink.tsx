6a5532cfd64caeefc2f979a3313023dbec6806ab
val <vfleurit@gmail.com>
2022-05-02 09:31:38 +0000

    enhance CopyLink particle

--- src/particles/CopyLink.tsx
+++ src/particles/CopyLink.tsx
@@ -1,33 +1,21 @@
-import { FC, HTMLProps, useEffect, useState } from "react";
+import { FC, HTMLProps } from "react";
 import toast from "react-hot-toast";
-
-import { omit } from "../utils";
 
 // Used to create a link that copies some text to clipboard
 const CopyLink: FC<
-  HTMLProps<HTMLAnchorElement> & {
+  Omit<HTMLProps<HTMLSpanElement>, "onClick"> & {
     textToCopy: string;
     infoDelay?: number;
   }
 > = (props) => {
-  let { textToCopy, infoDelay, className, ...filteredProps } = omit(
-    props,
-    "onClick"
-  );
+  let { textToCopy, infoDelay, ...filteredProps } = props;
 
   let handleClick = () =>
     navigator.clipboard.writeText(props.textToCopy).then(() => {
       toast("Copied !", { duration: 2e3 });
     });
 
-  return (
-    <span
-      {...filteredProps}
-      className={className}
-      children={props.children}
-      onClick={handleClick}
-    />
-  );
+  return <span onClick={handleClick} {...filteredProps} />;
 };
 
 export default CopyLink;
