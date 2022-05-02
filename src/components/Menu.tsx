042875805589a37b3c59171faa9e562841435a8a
val <vfleurit@gmail.com>
2022-05-02 09:32:08 +0000

    change redirect animation

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,19 +1,40 @@
-import { Link } from "react-router-dom";
+import TimedRouterLink from "../particles/TimedRouterLink";
 
 import "./Menu.scss";
 
-const Menu = () => (
-  <div className={"menu"}>
-    <Link className={"link"} to="/">
-      Home
-    </Link>
-    <Link className={"link"} to="/projects">
-      Projects
-    </Link>
-    <Link className={"link"} to="/social">
-      Social
-    </Link>
-  </div>
-);
+const Menu = () => {
+  let timeout = 500;
+  let onTimeoutStart = () =>
+    document.querySelector(".main")?.classList.add("disappearing");
+
+  return (
+    <div className={"menu"}>
+      <TimedRouterLink
+        to="/"
+        timeout={timeout}
+        onTimeoutStart={onTimeoutStart}
+        className={"link"}
+      >
+        Home
+      </TimedRouterLink>
+      <TimedRouterLink
+        to="/projects"
+        timeout={timeout}
+        onTimeoutStart={onTimeoutStart}
+        className={"link"}
+      >
+        Projects
+      </TimedRouterLink>
+      <TimedRouterLink
+        to="/social"
+        timeout={timeout}
+        onTimeoutStart={onTimeoutStart}
+        className={"link"}
+      >
+        Social
+      </TimedRouterLink>
+    </div>
+  );
+};
 
 export default Menu;
