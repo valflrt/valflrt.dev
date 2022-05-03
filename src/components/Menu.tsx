d8c5994d101e79e83e35f8f8f0e0f9fb178d22f7
val <vfleurit@gmail.com>
2022-05-03 07:07:32 +0000

    add tab focus in Menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,28 +1,58 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
 import TimedRouterLink from "../particles/TimedRouterLink";
+
+import { css } from "../utils";
 
 import "./Menu.scss";
 
 const Menu = () => {
+  let location = useLocation();
+
   let otherLinkProps = {
     timeout: 450,
     onTimeoutStart: () =>
       document
         .querySelector("#root > .layout > .main")
         ?.classList.add("disappearing"),
-    className: "link",
   };
 
   return (
     <div className={"menuWrapper"}>
       <div className={"menu"}>
         <div className={"links"}>
-          <TimedRouterLink to={"/"} {...otherLinkProps}>
-            Home
+          <TimedRouterLink
+            to={"/"}
+            className={css.j(
+              "link",
+              "noUnderlining",
+              location.pathname === "/" ? "focused" : ""
+            )}
+            {...otherLinkProps}
+          >
+            Main
           </TimedRouterLink>
-          <TimedRouterLink to={"/projects"} {...otherLinkProps}>
+          <TimedRouterLink
+            to={"/projects"}
+            className={css.j(
+              "link",
+              "noUnderlining",
+              location.pathname === "/projects" ? "focused" : ""
+            )}
+            {...otherLinkProps}
+          >
             Projects
           </TimedRouterLink>
-          <TimedRouterLink to={"/social"} {...otherLinkProps}>
+          <TimedRouterLink
+            to={"/social"}
+            className={css.j(
+              "link",
+              "noUnderlining",
+              location.pathname === "/social" ? "focused" : ""
+            )}
+            {...otherLinkProps}
+          >
             Social
           </TimedRouterLink>
         </div>
