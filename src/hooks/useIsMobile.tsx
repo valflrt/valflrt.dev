4ed745d6e6819a1e0988d9a5c0d3dde53cd11d3d
valflrt <vfleurit@gmail.com>
2022-05-03 20:18:03 +0200

    edit useIsMobile hook

--- src/hooks/useIsMobile.tsx
+++ src/hooks/useIsMobile.tsx
@@ -3,16 +3,11 @@ import { useState } from "react";
 import { useWindowEvent } from "./useWindowEvent";
 
 let useIsMobile = () => {
-  let condition = () =>
-    "ontouchstart" in window ||
-    navigator.maxTouchPoints > 0 ||
-    window.innerWidth < window.innerHeight + 100;
+  let condition = () => window.innerWidth < 700;
 
   let [isMobile, setIsMobile] = useState(condition());
 
-  useWindowEvent("resize", () => {
-    setIsMobile(condition());
-  });
+  useWindowEvent("resize", () => setIsMobile(condition()));
 
   return isMobile;
 };
