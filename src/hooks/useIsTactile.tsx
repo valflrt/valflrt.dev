ee35151dd5317389fed227e89228a9cc3b85bea3
valflrt <vfleurit@gmail.com>
2022-05-05 19:13:59 +0200

    add useIsTactile hook

---
+++ src/hooks/useIsTactile.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+import { useWindowEvent } from "./useWindowEvent";
+
+let useIsTactile = () => {
+  let condition = () => "ontouchstart" in window || navigator.maxTouchPoints;
+
+  let [isTactile, setIsTactile] = useState(condition());
+
+  useWindowEvent("resize", () => setIsTactile(condition()));
+
+  return isTactile;
+};
+
+export default useIsTactile;
