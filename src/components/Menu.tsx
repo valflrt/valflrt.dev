6e3b33d7806496c802294fff98c6d59d0cd31998
valflrt <vfleurit@gmail.com>
2022-05-15 12:27:22 +0200

    Edit Menu style

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -29,49 +29,51 @@ const Menu = () => {
   );
 
   return (
-    <div className={"menuWrapper"}>
-      <div
-        ref={toggleRef}
-        className={css.j("toggle", isMenuHidden ? "rotated" : "")}
-        onClick={() => setIsMenuHidden((s) => !s)}
-      >
-        <ArrowLeft />
-      </div>
-      <div className={css.j("menu", isMenuHidden ? "hidden" : "")}>
-        <div className={"links"}>
-          <TimedRouterLink
-            to={"/"}
-            className={css.j(
-              "link",
-              "noUnderlining",
-              useMatch({ path: "/", end: true }) ? "focused" : ""
-            )}
-            {...otherLinkProps}
-          >
-            Main
-          </TimedRouterLink>
-          <TimedRouterLink
-            to={"/projects"}
-            className={css.j(
-              "link",
-              "noUnderlining",
-              useMatch({ path: "/projects", end: true }) ? "focused" : ""
-            )}
-            {...otherLinkProps}
-          >
-            Projects
-          </TimedRouterLink>
-          <TimedRouterLink
-            to={"/social"}
-            className={css.j(
-              "link",
-              "noUnderlining",
-              useMatch({ path: "/social", end: true }) ? "focused" : ""
-            )}
-            {...otherLinkProps}
-          >
-            Social
-          </TimedRouterLink>
+    <div className={"menuWrapperWrapper"}>
+      <div className={"menuWrapper"}>
+        <div
+          ref={toggleRef}
+          className={css.j("toggle", isMenuHidden ? "rotated" : "")}
+          onClick={() => setIsMenuHidden((s) => !s)}
+        >
+          <ArrowLeft />
+        </div>
+        <div className={css.j("menu", isMenuHidden ? "hidden" : "")}>
+          <div className={"links"}>
+            <TimedRouterLink
+              to={"/"}
+              className={css.j(
+                "link",
+                "noUnderlining",
+                useMatch({ path: "/", end: true }) ? "focused" : ""
+              )}
+              {...otherLinkProps}
+            >
+              Main
+            </TimedRouterLink>
+            <TimedRouterLink
+              to={"/projects"}
+              className={css.j(
+                "link",
+                "noUnderlining",
+                useMatch({ path: "/projects", end: true }) ? "focused" : ""
+              )}
+              {...otherLinkProps}
+            >
+              Projects
+            </TimedRouterLink>
+            <TimedRouterLink
+              to={"/social"}
+              className={css.j(
+                "link",
+                "noUnderlining",
+                useMatch({ path: "/social", end: true }) ? "focused" : ""
+              )}
+              {...otherLinkProps}
+            >
+              Social
+            </TimedRouterLink>
+          </div>
         </div>
       </div>
     </div>
