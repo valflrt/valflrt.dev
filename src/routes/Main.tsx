9f970550b9afc8b3f75dcfb863bca72c432af857
valflrt <vfleurit@gmail.com>
2022-05-22 12:34:43 +0200

    Add elapsed life time in Main.ts

--- src/routes/Main.tsx
+++ src/routes/Main.tsx
@@ -5,11 +5,23 @@ import { css } from "../utils";
 const Main = () => {
   document.title = "Home – valflrt.dev";
 
+  let divide = (x: number, y: number) => [Math.floor(x / y), x % y];
+
+  let getElapsedTime = () => {
+    let minutesQ = divide(Date.now() / 1000 - 1108087200, 60);
+    let hoursQ = divide(minutesQ[0], 60);
+    let daysQ = divide(hoursQ[0], 24);
+    let yearsQ = divide(daysQ[0], 365);
+
+    return `${yearsQ[0].toFixed()} years, ${yearsQ[1].toFixed()} days and ${daysQ[1].toFixed()} hours`;
+  };
+
   return (
     <div className={"main"}>
       <h1>Heya ! I'm valflrt !</h1>
       <ul>
         <li>A coding and self-deprecation enthusiast</li>
+        <li>My real name is Valentin Fleurit</li>
         <li>
           I was born at{" "}
           <CopyLink
@@ -20,6 +32,7 @@ const Main = () => {
           </CopyLink>{" "}
           (Unix Epoch)
         </li>
+        <li>I've been alive for {getElapsedTime()}</li>
         <li>I'm French and proud to be (oui oui baguette)</li>
         <li>I like TypeScript</li>
       </ul>
