0e1047cbdb43e3770b1325a774a98dfcdb792786
valflrt <vfleurit@gmail.com>
2022-05-30 19:44:54 +0200

    Remove misc.ts

--- src/misc.ts
+++
@@ -1,7 +0,0 @@
-export const TimedRouterLinkDefaultProps = {
-  timeout: 450,
-  onTimeoutStart: () =>
-    document
-      .querySelector("#root > .layout > .main")
-      ?.classList.add("disappearing"),
-};
