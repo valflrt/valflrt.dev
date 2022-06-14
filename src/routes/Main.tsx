c46a651db403aa9a03ef79e2b133f054a64b560e
valflrt <vfleurit@gmail.com>
2022-06-14 12:26:12 +0200

    Edit Main Route

--- src/routes/Main.tsx
+++ src/routes/Main.tsx
@@ -3,31 +3,27 @@ import CopyLink from "../common/CopyLink";
 const Main = () => {
   document.title = "Home – valflrt.dev";
 
-  let divide = (x: number, y: number) => [Math.floor(x / y), x % y];
-
-  let getElapsedTime = () => {
-    let minutesQ = divide(Date.now() / 1000 - 1108087200, 60);
-    let hoursQ = divide(minutesQ[0], 60);
-    let daysQ = divide(hoursQ[0], 24);
-    let yearsQ = divide(daysQ[0], 365);
-
-    return `${yearsQ[0].toFixed()} years and ${yearsQ[1].toFixed()} days`;
-  };
+  let elapsedDays = (Date.now() / 1000 - 1108087200) / 60 / 60 / 24;
 
   return (
     <>
       <h1>Heya ! I'm valflrt !</h1>
       <ul>
-        <li>A coding and self-deprecation enthusiast</li>
+        <li>A coding enthusiast</li>
         <li>My real name is Valentin Fleurit</li>
         <li>
           I was born at{" "}
-          <CopyLink className={"link"} textToCopy={"1108087200"}>
+          <CopyLink
+            className={"link"}
+            textToCopy={"1108087200"}
+            title={`Alive for ${(elapsedDays / 365).toFixed()} years and ${(
+              elapsedDays % 365
+            ).toFixed()} days`}
+          >
             <code>1108087200</code>
           </CopyLink>{" "}
           (Unix Epoch)
         </li>
-        <li>I've been alive for {getElapsedTime()}</li>
         <li>I'm French and proud to be (oui oui baguette)</li>
         <li>I like TypeScript</li>
       </ul>
