79707008a20694e6e236d0e2931bc44a2e59bb57
valflrt <vfleurit@gmail.com>
2022-06-19 17:15:20 +0200

    Change ExternalLinks Route style

--- src/routes/ExternalLinks.tsx
+++ src/routes/ExternalLinks.tsx
@@ -2,6 +2,9 @@ import Link from "../common/Link";
 import CopyLink from "../common/CopyLink";
 
 import { css } from "../utils";
+import { AtSign, GitHub } from "react-feather";
+
+import "./ExternalLinks.scss";
 
 const ExternalLinks = () => {
   document.title = "External Links – valflrt.dev";
@@ -9,33 +12,27 @@ const ExternalLinks = () => {
   return (
     <>
       <h1>External Links</h1>
-      <ul>
-        <li>
-          <Link
-            className={css.j("link", "underlined")}
-            to={"https://gh.valflrt.dev"}
-            target={"_blank"}
-          >
-            Github account
-          </Link>
-        </li>
-        <li>
-          <CopyLink
-            className={css.j("link", "underlined")}
-            textToCopy={"valflrt#8436"}
-          >
-            Discord: valflrt#8436
-          </CopyLink>
-        </li>
-        <li>
-          <CopyLink
-            className={css.j("link", "underlined")}
-            textToCopy={"val@valflrt.dev"}
-          >
-            Email: val@valflrt.dev
-          </CopyLink>
-        </li>
-      </ul>
+      <div className="links">
+        <Link
+          className={css.j("link", "underlined")}
+          to={"https://gh.valflrt.dev"}
+          target={"_blank"}
+        >
+          <GitHub /> Github account
+        </Link>
+        <CopyLink
+          className={css.j("link", "underlined")}
+          textToCopy={"valflrt#8436"}
+        >
+          Discord: valflrt#8436
+        </CopyLink>
+        <CopyLink
+          className={css.j("link", "underlined")}
+          textToCopy={"val@valflrt.dev"}
+        >
+          <AtSign /> Email: val@valflrt.dev
+        </CopyLink>
+      </div>
     </>
   );
 };
