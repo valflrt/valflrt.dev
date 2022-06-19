ef9a58819ab7e26fe90a71c5611a2df275003c48
valflrt <vfleurit@gmail.com>
2022-06-19 17:10:46 +0200

    Improve RouterLink

--- src/common/RouterLink.tsx
+++ src/common/RouterLink.tsx
@@ -1,7 +1,20 @@
-import { Link, LinkProps } from "react-router-dom";
+import { Link, LinkProps, useMatch } from "react-router-dom";
 
-const RouterLink: React.FC<LinkProps> = (props) => {
-  return <Link {...props} />;
+const RouterLink: React.FC<
+  Omit<LinkProps, "className" | "to"> & {
+    to: string;
+    className: (isFocused: boolean) => string;
+  }
+> = (props) => {
+  let { className, ...filteredProps } = props;
+  let isFocused = !!useMatch({ path: props.to, end: true });
+
+  return (
+    <Link
+      className={className ? className(isFocused) : undefined}
+      {...filteredProps}
+    />
+  );
 };
 
 export default RouterLink;
