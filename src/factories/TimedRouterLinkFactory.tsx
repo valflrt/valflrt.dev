d178a512accc3e02ea09e361eeed6cb8cec568ea
valflrt <vfleurit@gmail.com>
2022-06-19 17:14:50 +0200

    Clean TimedRouterLinkFactory

--- src/factories/TimedRouterLinkFactory.tsx
+++ src/factories/TimedRouterLinkFactory.tsx
@@ -1,21 +1,8 @@
 import React from "react";
-import { LinkProps } from "react-router-dom";
 
 import TimedRouterLink, {
   TimedRouterLinkProps,
 } from "../common/TimedRouterLink";
-
-let customProps = {
-  timeout: 450,
-  onTimeoutStart: () =>
-    document
-      .querySelector("#root > .layout > .main")
-      ?.classList.add("disappearing"),
-  onTimeoutEnd: () =>
-    document
-      .querySelector("#root > .layout > .main")
-      ?.classList.remove("disappearing"),
-};
 
 const TimedRouterLinkFactory = <T extends Omit<TimedRouterLinkProps, "to">>(
   customProps: T
@@ -27,9 +14,3 @@ const TimedRouterLinkFactory = <T extends Omit<TimedRouterLinkProps, "to">>(
 };
 
 export default TimedRouterLinkFactory;
-
-/* const CustomTimedRouterLink: React.FC<
-  Omit<TimedRouterLinkProps, "timeout" & "onTimeoutStart" & "onTimeoutEnd">
-> = (props) => {
-  return <TimedRouterLink {...customProps} {...props} />;
-}; */
