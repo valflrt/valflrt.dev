15ae1f384155b211985cb13c024a46d5d3eaffd3
valflrt <vfleurit@gmail.com>
2022-06-19 18:48:44 +0200

    Add "see more on github" in Projects Route

--- src/routes/Projects.tsx
+++ src/routes/Projects.tsx
@@ -11,15 +11,22 @@ const Projects = () => {
   return (
     <>
       <h1>Some of my Projects</h1>
-      <div className="projects">
+      <div className="list">
         {projects.map((p, i) => (
-          <Link className={css.j("link")} to={p.link} target={"_blank"} key={i}>
-            <div className="projectCard">
+          <Link className={"link item"} to={p.link} target={"_blank"} key={i}>
+            <div className="card">
               <div className="underlined">{p.name}</div>
               <div className="description">{p.description}</div>
             </div>
           </Link>
         ))}
+        <Link
+          to={"https://gh.valflrt.dev?tab=repositories"}
+          target={"_blank"}
+          className={"link item"}
+        >
+          <div className={"more underlined"}>See more on github...</div>
+        </Link>
       </div>
     </>
   );
