2f7f5c2ea668a2a1df423314a0fc88b2fa07b64a
valflrt <vfleurit@gmail.com>
2022-06-19 16:15:01 +0200

    Change Projects Route style

--- src/routes/Projects.tsx
+++ src/routes/Projects.tsx
@@ -1,6 +1,9 @@
+import projects from "../assets/projects";
 import Link from "../common/Link";
 
 import { css } from "../utils";
+
+import "./Projects.scss";
 
 const Projects = () => {
   document.title = "Projects – valflrt.dev";
@@ -8,35 +11,16 @@ const Projects = () => {
   return (
     <>
       <h1>Some of my Projects</h1>
-      <ul>
-        <li>
-          <Link
-            className={css.j("link", "underlined")}
-            to={"https://gh.valflrt.dev/lejardinier"}
-            target={"_blank"}
-          >
-            Le Jardinier (discord bot)
+      <div className="projects">
+        {projects.map((p, i) => (
+          <Link className={css.j("link")} to={p.link} target={"_blank"} key={i}>
+            <div className="projectCard">
+              <div className="underlined">{p.name}</div>
+              <div className="description">{p.description}</div>
+            </div>
           </Link>
-        </li>
-        <li>
-          <Link
-            className={css.j("link", "underlined")}
-            to={"https://gh.valflrt.dev/Fencryption"}
-            target={"_blank"}
-          >
-            Fencryption (A directory encrypting tool)
-          </Link>
-        </li>
-        <li>
-          <Link
-            className={css.j("link", "underlined")}
-            to={"https://gh.valflrt.dev/react-template"}
-            target={"_blank"}
-          >
-            A React template
-          </Link>
-        </li>
-      </ul>
+        ))}
+      </div>
     </>
   );
 };
