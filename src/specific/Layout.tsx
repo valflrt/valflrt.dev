bdd33ecf769be0ad0451ca6828bfe9f53240ce0e
valflrt <vfleurit@gmail.com>
2022-06-20 11:47:16 +0200

    Add Spinner in Layout

--- src/specific/Layout.tsx
+++ src/specific/Layout.tsx
@@ -4,6 +4,8 @@ import { Toaster } from "react-hot-toast";
 import useIsMobile from "../hooks/useIsMobile";
 import useIsTactile from "../hooks/useIsTactile";
 
+import Spinner from "./Spinner";
+
 import Menu from "./Menu";
 
 import { css } from "../utils";
@@ -21,6 +23,7 @@ const Layout: React.FC = ({ children }) => (
     <Toaster position={"top-right"} toastOptions={{ className: "toast" }} />
     {children}
     <Menu />
+    <Spinner />
   </div>
 );
 
