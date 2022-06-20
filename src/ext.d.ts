102d519d767f2109b93d57c26e12950376822968
valflrt <vfleurit@gmail.com>
2022-06-20 11:48:03 +0200

    .

---
+++ src/ext.d.ts
@@ -0,0 +1,13 @@
+/**
+ * Those are declarations of the file types allowed for
+ * import
+ */
+declare module "*.css";
+declare module "*.scss";
+declare module "*.sass";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.png";
+declare module "*.gif";
+declare module "*.mp3";
+declare module "*.svg";
