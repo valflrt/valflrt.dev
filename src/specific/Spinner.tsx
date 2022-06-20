d5a53d81101eefa5be35f15830ab9c4112786434
valflrt <vfleurit@gmail.com>
2022-06-20 15:26:33 +0200

    Edit Spinner

--- src/specific/Spinner.tsx
+++ src/specific/Spinner.tsx
@@ -4,7 +4,18 @@ import "./Spinner.scss";
 
 const Spinner: React.FC = () => (
   <div className="spinner">
-    <span></span>
+    <svg viewBox="25 25 50 50">
+      <circle
+        className="path"
+        cx="50"
+        cy="50"
+        r="20"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="8"
+        strokeMiterlimit="10"
+      />
+    </svg>
   </div>
 );
 
