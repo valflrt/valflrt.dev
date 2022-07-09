2837cf060d44d8c72ba016064d6087a14e7c4d37
valflrt <vfleurit@gmail.com>
2022-07-09 15:28:40 +0200

    Misc change in useTheme

--- src/hooks/useTheme.tsx
+++ src/hooks/useTheme.tsx
@@ -11,7 +11,7 @@ let useTheme = () => {
   }, [theme]);
 
   let switchTheme = () => {
-    setTheme((os) => (os === "light" ? "dark" : "light"));
+    setTheme((old) => (old === "light" ? "dark" : "light"));
   };
 
   return [theme, switchTheme] as ["light" | "dark", () => void];
